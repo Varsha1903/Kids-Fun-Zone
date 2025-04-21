eb2addcc1b3d58c5aae777688765bac7a5f36c0f
Varsh <135642044+Varsha1903@users.noreply.github.com>
2025-04-21 23:30:48 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function openDashboard() {
+  window.open("https://app.powerbi.com/links/tpVHoOB_dn?ctid=562673cb-3a12-4428-9c2e-82d74fd5889a&pbi_source=linkShare", "_blank");
+}
+function openPredictionModel() {
+  window.open("https://colab.research.google.com/drive/11CxrrmifLC0fNAT_Bc_sTcOlySr74Y7y#scrollTo=GHueKCP8CNI0", "_blank");
+}
